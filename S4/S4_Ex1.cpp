3b011df9fb54c8d72632696eee31507a3c12864b
Zaira98742 <ngducanh98742@gmail.com>
2024-11-25 10:33:55 +0700

    Add files via upload

---
+++ S4/S4_Ex1.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+	int SoNguyen;
+	printf("nhap vao 1 so nguyen ");
+	scanf("%d", &SoNguyen);
+	if(SoNguyen >= 1){
+		printf("day la so duong");
+		} else{
+			printf("day la so am");
+		}
+	return 0;
+	}
