86e3e5640c57ca470c3d2cd9ac09178d55272d91
Zaira98742 <ngducanh98742@gmail.com>
2024-11-26 13:05:01 +0700

    Add files via upload

---
+++ S5/S5-Ex2.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+	int number = 30;
+	int choseYourNumber;
+	while (number != choseYourNumber) {	
+	printf("Moi ban nhap mot so (goi y la so nho hon 50 lon hon 10) : ");
+	scanf("%d", &choseYourNumber );
+}
+    printf("%d la dap an chinh xac", number); 
+	return 0;
+}
