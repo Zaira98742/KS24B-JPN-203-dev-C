10d7b0678ca6d60a7d61fe96ded0205efc9e350f
Zaira98742 <ngducanh98742@gmail.com>
2024-11-29 12:09:55 +0700

    Add files via upload

---
+++ S7/S7-Ex4.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int length;
+    printf("Please input length of the array: \n");
+    scanf("%d", &length);
+    int array[length];
+    for(int i = 0; i <= length - 1; i = i + 1) {
+        printf("Please input array[%d]: ", i);
+        scanf("%d", &array[i]);
+    }
+    for(int i = 0; i <= length - 1; i = i + 1) {
+        printf("Array[%d] = %d\n", i, array[i]);
+    }
+	return 0;
+	}
